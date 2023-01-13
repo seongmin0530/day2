7f682b6977cdec14b7831aa840d4875e7046b936
kimseongmin <kpmgksm@naver.com>
2023-01-13 13:37:05 +0900

    variable

--- day02.py
+++ day02.py
@@ -1,2 +1,15 @@
-PI = 3.14
-print(f'원주율의 값은 {PI}이고 타입은 {type(PI)}입니다.')
+# chapter 3
+print(2*2*2*2*2)
+print(2**5)#2의 5제곱
+print(pow(2, 5))#2의 5제곱
+print(divmod(9, 5))#9를 5로 나눈 몫과 나머지
+print(type(divmod(9, 5)))#tuple
+print(type((1, 2)))#tuple
+
+test = 1, 2 #packing
+print(type(test))#tuple
+print(test)
+print(test[1])
+a, b = test #unpacking
+print(a, b)
+print(type(a))
