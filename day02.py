e64816b68d07e5feab15a1e980613c9023aee909
kimseongmin <kpmgksm@naver.com>
2023-01-13 10:55:25 +0900

    day02 datatype dictionary

---
+++ day02.py
@@ -0,0 +1,7 @@
+harry_porter = {'헤르미온느':'그리핀도르', '말포이':'슬리데린', '말포이': '그리핀도르'}
+print(harry_porter)
+print(type(harry_porter))
+#딕셔너리 => 같은 이름의 키를 갖는 요소는 마지막에 저장한 값으로만 저장됨
+harry_porter = {'헤르미온느':'그리핀도르', '말포이':'슬리데린', '론': '그리핀도르'}
+print(harry_porter)
+print(type(harry_porter))
